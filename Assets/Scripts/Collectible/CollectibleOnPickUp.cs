6e7dbe6352379f158c285786a7bf13c169a9df51
borqe <paulauskx@gmail.com>
2019-03-06 11:28:59 +0200

    Ideti win/lose states, eina paimti obuoli, uzsiskaito taskai

---
+++ Assets/Scripts/Collectible/CollectibleOnPickUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleOnPickUp : MonoBehaviour
+{
+    GameObject player;
+    PlayerScore playerScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScore = player.GetComponent<PlayerScore>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Destroy(this.gameObject);
+            player.GetComponent<Renderer>().material.color = Color.green;
+            playerScore.currentScore++;
+        }
+    }
+}
