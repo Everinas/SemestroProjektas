edfbf027c4a8690939fc6d7729dbeffacd0ae6ed
lukonic <48091019+lukonic@users.noreply.github.com>
2019-03-17 01:33:13 +0200

    Added a chest. Approaching it without keys will play a "shake box" animation. Finding a key and bringing it to the box will open the box; some spring cleaning

---
+++ Assets/Scripts/NPC/chest_opening.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chest_opening : MonoBehaviour
+{
+    GameObject player;
+    Animator anim;
+    PlayerScore playerScore;
+    private bool opened;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
+        playerScore = player.GetComponent<PlayerScore>();
+        opened = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            if (opened == false)
+            {
+                anim.Play("Shake");
+                print("Veikia");
+                if (PlayerScore.keys > 0)
+                {
+                    playerScore.currentKeys--;
+                    anim.Play("Open");
+                    opened = true;
+                }
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && opened == false)
+        {
+            anim.Play("New State");
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerScore.cs
+++ Assets/Scripts/Player/PlayerScore.cs
@@ -8,20 +8,28 @@ public class PlayerScore : MonoBehaviour
     //Set up a variable to store how many you've collected
     private int startScore = 0;
     public int currentScore;
+    private int startKeys = 0;
+    public int currentKeys;
     public static int score; //Needed for NPC_dialogue
+    public static int keys; //Needed for NPC_dialogue
     public Text counter;
+    public Text Keys;
     private GameObject player;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         currentScore = startScore;
+        currentKeys = startKeys;
         counter.text = "Score: 0";
+        Keys.text = "Keys: 0";
     }
 
     private void Update()
     {
         counter.text = "Score: " + currentScore.ToString();
+        Keys.text = "Keys: " + currentKeys.ToString();
         score = currentScore;
+        keys = currentKeys;
     }
 }
