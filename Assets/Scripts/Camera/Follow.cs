2afd78c2909d28b48e7dfcb0fbe6083db53836ca
lukonic <48091019+lukonic@users.noreply.github.com>
2019-03-16 20:24:57 +0200

    Proper god damn camera!!!

--- Assets/Scripts/Camera/Follow.cs
+++ Assets/Scripts/Camera/Follow.cs
@@ -1,26 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class Follow : MonoBehaviour
 {
-    public float turnSpeed = 4.0f;
-    public Transform player;
 
-    private Vector3 offset;
+    public bool lockCursor;
+    public float mouseSensitivity = 10;
+    public Transform target;
+    public float dstFromTarget = 2;
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
 
+    public float rotationSmoothTime = .12f;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
 
+    float yaw;
+    float pitch;
 
     void Start()
     {
-        offset = new Vector3(5, 5 , 5 );
-        Cursor.lockState = CursorLockMode.Locked;
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
 
     void LateUpdate()
     {
-        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
-        transform.position = player.position + offset;
-        transform.LookAt(player.position);
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+        transform.eulerAngles = currentRotation;
+
+        transform.position = target.position - transform.forward * dstFromTarget;
+
     }
+
 }
