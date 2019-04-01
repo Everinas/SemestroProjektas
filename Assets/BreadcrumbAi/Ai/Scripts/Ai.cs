3baa31761653ce23b26c56c087010e8716eb9848
lukonic <48091019+lukonic@users.noreply.github.com>
2019-04-01 07:22:09 +0300

    Created a script to destroy items after specified time. (DestroyOverTime). Made the ranged enemy disappear soon after its death.

--- Assets/BreadcrumbAi/Ai/Scripts/Ai.cs
+++ Assets/BreadcrumbAi/Ai/Scripts/Ai.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 namespace BreadcrumbAi{
 	[System.Serializable]
 	public class Ai : MonoBehaviour {
-		
+        DestroyOverTime script;
 		#region Editor Variables
 		// *****EDITOR VARIABLES***** //
 		public bool _CanFollowPlayer, _CanFollowBreadcrumbs, _CanFollowAi, _CanWander, _CanPatrol,
@@ -18,6 +18,7 @@ namespace BreadcrumbAi{
 					_IsMelee, _IsRanged,
 					_IsGround, _IsAir,
 					_IsInvincible;
+
 					
 		public float followSpeed, wanderSpeed, patrolSpeed, rotationSpeed, avoidSpeed,
 					 jumpDistance, jumpForce, longJumpForce,
@@ -27,7 +28,6 @@ namespace BreadcrumbAi{
 					 Health;
         GameObject player;
         #endregion
-
         // States are used for adding actions, animations, sounds, etc to your Ai.
         #region STATES
         public enum LIFE_STATE{
@@ -423,10 +423,11 @@ namespace BreadcrumbAi{
 		}
 
         public void OnTriggerEnter(Collider other)
-        {
+        {   
             if (other.gameObject == player)
             {
                 Health--;
+                this.GetComponent<DestroyOverTime>().enabled = true;
             }
         }
 	}
