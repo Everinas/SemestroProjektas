290df89ca8e7f488dcb5b8e1c47d150ed17a397a
lukonic <48091019+lukonic@users.noreply.github.com>
2019-04-10 03:02:09 +0300

    NPC now looks at you when you speak to him, follows you while you are in his proximity

--- Assets/Scripts/NPC/NPC_Dialogue.cs
+++ Assets/Scripts/NPC/NPC_Dialogue.cs
@@ -12,10 +12,17 @@ public class NPC_Dialogue : MonoBehaviour
     public static bool levelchange = false;
     public static bool buttonPress = false;
     public static bool wave = false;
+    Collider player;
     private void OnTriggerStay(Collider other)
     {
         if (Input.GetButtonDown("E") && buttonPress == false)
         {
+            player = other;
+            //Vector3 targetPostition = new Vector3(other.transform.position.x,
+            //                           this.transform.position.y,
+            //                           other.transform.position.z);
+            //this.transform.LookAt(targetPostition);
+            
             if (wave == false)
             {
                 wave = true;
@@ -50,6 +57,27 @@ public class NPC_Dialogue : MonoBehaviour
             buttonPress = false;
             dialogue.text = "";
             wave = false;
+        }
+    }
+    void start()
+    {
+        Rigidbody mm_Rigidbody;
+        mm_Rigidbody = GetComponent<Rigidbody>();
+        //This locks the RigidBody so that it does not move or rotate in the Z axis.
+        mm_Rigidbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ;
+    }
+     void Update()
+    {
+        if (wave == true)
+        {
+            Vector3 targetDir = player.transform.position - transform.position;
+            targetDir.y = 0;
+            float step = 2 * Time.deltaTime;
+
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+            Debug.DrawRay(transform.position, newDir, Color.red);
+
+            transform.rotation = Quaternion.LookRotation(newDir);
         }
     }
 }
