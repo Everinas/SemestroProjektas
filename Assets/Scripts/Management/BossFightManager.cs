fbb01285643d0bb487bc344ec4f4d72f907ec9ed
borqe <paulauskx@gmail.com>
2019-05-20 01:31:04 +0300

    Created most of the level design
    - Added a place to collect apples
    - Added a place to get the key
    - Added a place to get the shield
    - Added the boss arena
    - Added a portal thing to the boss arena
    - Started a boss manager script - only teleports the player for now

---
+++ Assets/Scripts/Management/BossFightManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFightManager : MonoBehaviour
+{
+    // This script should:
+
+    // During the fight: spawn the teleport points which teleport you to the top of the arena
+    // From there, you can jump on top of the boss and damage it
+    // On win: change the lighting back to normal, and teleport the npc to the center of the arena
+
+}
---
+++ Assets/Scripts/Management/BossLoadManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossLoadManager : MonoBehaviour
+{
+    public CameraFollow cameraMovement;
+    public GameObject player;
+    public GameObject teleportPoint;
+
+    void OnTriggerStay()
+    {
+        if (Input.GetButtonDown("E"))
+        {
+            if (NPC_Dialogue.levelchange == true)
+            {
+                // Teleport the player to the boss zone
+                player.transform.position = teleportPoint.transform.position;
+                cameraMovement = GameObject.FindGameObjectWithTag("CameraFolder").GetComponent<CameraFollow>();
+                cameraMovement.enabled = false;
+                cameraMovement.enabled = true;
+
+                // Change the lighting and enable the boss
+            }
+        }
+    }
+    
+}
